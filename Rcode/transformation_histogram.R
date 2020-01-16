904889cd1ed4c17506aebc0d18c5c450f40eb350
shuangj00 <shela0519@gmail.com>
2020-01-16 11:54:26 -0600

    update Rcode

--- Rcode/transformation_histogram.R
+++ Rcode/transformation_histogram.R
@@ -10,7 +10,7 @@ load("Data_NielsenHB_real.Rdata", verbose = T)
 
 # case group #
 abd.raw = case.real.spec
-abd.trans = log(1 + 1000*abd.raw) / log(1 + abd.raw)
+abd.trans = log((1 + 1000*abd.raw) / (1 + abd.raw))
 abd.trans[is.na(abd.trans)] = 0
 
 abd.raw.v = apply(abd.raw, 2, mean)
@@ -29,5 +29,16 @@ ggplot(subset(case.abd.df, Type == "Original"), aes(x=Abundance)) +
 ggplot(subset(case.abd.df, Type == "Transformed"), aes(x=Abundance)) + 
   geom_histogram( bins = 30)+ylim(0, 620)+
   xlab("Transformed abundance")
+
+# get the transformation function #
+x = seq(0, 1, by = 0.0001)
+x.trans = log(1 + 1000*x ) / log(1 + x )
+x.trans = log((1 + 1000*x ) / (1+x))
+par(mar = c(5, 7, 2, 2))
+plot(x, x.trans, type = "l",
+     ylab = expression(paste("log ", frac("1+1000x", "1+x"))), 
+     main = "Abundance transformation")
+
+
 
 
